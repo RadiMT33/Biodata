14185bae054e276af387ad9f3429a58e4846c185
RadiMaulanaTsaqif <113565405+RadiMT33@users.noreply.github.com>
2023-10-10 20:02:43 +0700

    Add files via upload

---
+++ Latihan1.js
@@ -0,0 +1,12 @@
+let biodata = "Welcome my biodata"
+document.getElementById("biodata").innerHTML = biodata;
+let nama = "RADI MAULANA TSAQIF"
+document.getElementById("nama").innerHTML = nama;
+let kelas = "XI PPLG 1"
+document.getElementById("kelas").innerHTML = kelas;
+let noHP = "083844108250"
+document.getElementById("noHP").innerHTML = noHP;
+let Alamat = "KAMPUNG DALEUM"
+document.getElementById("Alamat").innerHTML = Alamat;
+let ttl = "TASIKMALAYA, 05 JANUARI 2007"
+document.getElementById("ttl").innerHTML = ttl;
